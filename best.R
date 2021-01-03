ebbffb8fbfa6740b45b8c296900c95f71dca5024
iampossiblyatwork <james@costellomail.net>
2021-01-03 02:01:38 -0500

    Final working version.

--- best.R
+++ best.R
@@ -1,6 +1,6 @@
-## best function takes 2 arguments (state and outcome) and rturns a chr
+## best function takes 2 arguments (state and outcome) and returns a chr
 ## vector with the name of the hospital that has the best 30-day mortality
-## for the speicifed outcome in that state.
+## for the specified outcome in that state.
 
 best<- function(state,outcome){
   ## Read outcome data
@@ -8,6 +8,10 @@ best<- function(state,outcome){
   ## Extract only pertinent data. Name, State, and all 3 rates.
   reduced <- full_data[,c(2,7,11,17,23)] 
   ## Check outcome and state is valid
+  ## each if statement first checks if the outcome is valid
+  ## then state is checked within each outcome check
+  ## finally the appropriate outcome is pulled from reduced dataset
+  ## this small dataset is that passed to maxfinder function
 
   if(outcome == "heart attack"){
     if (state %in% reduced$State){
@@ -18,14 +22,14 @@ best<- function(state,outcome){
     }
   } else if(outcome == "pneumonia"){
     if (state %in% reduced$State){
-        reducedbyoutcome<-reduced[,c(1,2,4)]
+        reducedbyoutcome<-reduced[,c(1,2,5)]
         return(maxfinder(reducedbyoutcome,state))
     } else{
       stop("invalid state")
     }
   } else if(outcome == "heart failure"){
     if (state %in% reduced$State){
-        reducedbyoutcome<-reduced[,c(1,2,5)]
+        reducedbyoutcome<-reduced[,c(1,2,4)]
         return(maxfinder(reducedbyoutcome,state))
     } else{
       stop("invalid state")
@@ -38,9 +42,14 @@ best<- function(state,outcome){
   ##rate
 }
 
-maxfinder <- function(data,state){
-  outcome_data <- data[which(data$State==state),]
-  best_rate <- colMax(outcome_data)
-  output <- best_rate
-  return(output)
+## simplifies code by finding min hospital mortality and returning Name
+maxfinder <- function(data,state_abbrv){
+  ## filters out all states not passed to function
+  outcome_data <- data[data$State == state_abbrv,]
+  ##calculates min based on 3rd col which is form sorted data
+  best_rate <- suppressWarnings(outcome_data[which.min(outcome_data[,3]),])
+  output <- best_rate[order(best_rate$Hospital.Name),] ##sorts multiple outcomes
+  ##grabs first line of sorted data
+  output <- output[1,]
+  return(output$Hospital.Name)
 }
